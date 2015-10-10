ced45398e1743000506add4a637cd68437c0ea2e
Jon Daniel <binarycleric@gmail.com>
2015-10-10 00:58:39 -0400

    Added things about computers.
    Bot will look for phrases about "how do computers" even and respond
    appropriately. Need to add tests and a bit more randomness in the future but
    this is good enough for version one.
    
    Got to learn Clojure so that was neat.

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"] 
+                 [twitter-api "0.7.8"]
+                 [twitter-streaming-client "0.3.3"]
+                 [org.clojure/tools.logging "0.3.1"]]
   :main ^:skip-aot hypertechnical.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
