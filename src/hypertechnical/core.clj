7bfd8d8e402b5b9b27cacd61627d83a57f596bec
Jon Daniel <binarycleric@gmail.com>
2015-10-26 20:17:23 -0400

    Refactoring to break things into smaller functions.
    Trying to get better at this lisp-y stuff.

--- src/hypertechnical/core.clj
+++ src/hypertechnical/core.clj
@@ -18,6 +18,12 @@
    (twitter.callbacks.protocols SyncSingleCallback))
   (:gen-class))
 
+(def search-terms
+  "computers how do they even, how do computers even")
+
+(def search-patterns
+  #{#"[cC]omputers[,]?\show do they even[\?]?" #"[hH]ow do computers even[\?]?$"})
+
 (def twitter-credentials 
   (make-oauth-creds (System/getenv "TWITTER_CONSUMER_KEY")
                     (System/getenv "TWITTER_CONSUMER_SECRET")
@@ -25,33 +31,37 @@
                     (System/getenv "TWITTER_ACCESS_TOKEN_SECRET")))
 
 (def stream
-  (let [search_terms "computers how do they even, how do computers even"] 
-    (client/create-twitter-stream statuses-filter :oauth-creds twitter-credentials 
-                                                  :params {:track search_terms})))
+  (client/create-twitter-stream 
+    statuses-filter :oauth-creds twitter-credentials 
+                    :params {:track search-terms}))
 
-(defn handle-matching-tweet [tweet]
-  (let [screen_name (:screen_name (:user tweet))
-        status_id (:id tweet)
-        message (str "@" screen_name " Computers even by setting the lowest order bit to False, duh!")]
+(defn response-text [screen_name]
+  (str "@" screen_name " Computers even by setting the lowest order bit to False, duh!"))
 
-    (log/info "Sending" message "to" screen_name ". status_id:" status_id)
+(defn tweet-response [tweet]
+  (let [screen-name (:screen_name (:user tweet))
+        status-id (:id tweet)
+        message (response-text screen-name)] 
+
+    (log/info "Sending" message "to" screen-name ". status_id:" status-id)
     (statuses-update :oauth-creds twitter-credentials
-                     :params {:status message :in_reply_to_status_id status_id}))) 
+                     :params {:status message :in_reply_to_status_id status-id}))) 
 
 (defn about-computers-evening? [tweet]
-  (let [message (:text tweet)
-        patterns #{#"[cC]omputers[,]?\show do they even[\?]?" #"[hH]ow do computers even[\?]?$"}]
+  (let [message (:text tweet)]
 
-    (not-empty (select (fn [pattern] (re-find pattern message)) patterns))))
+    (not-empty 
+      (select (fn [pattern] (re-find pattern message)) search-patterns))))
+
+; TODO: figure out better names for all this.
+(defn process-tweets [tweets]
+  (let [filtered-tweets (select about-computers-evening? tweets)]
+    (if-not (nil? filtered-tweets)
+      (doseq [t filtered-tweets] (tweet-response t)))))
 
 (defn -main [& args]
   (client/start-twitter-stream stream)
 
-  (while 
-    true
-    (time (Thread/sleep (* 1024 5)))
-    (let [raw-tweets (:tweet (client/retrieve-queues stream))
-          filtered-tweets (select about-computers-evening? raw-tweets)]
-
-      (if-not (nil? filtered-tweets)
-        (doseq [t filtered-tweets] (handle-matching-tweet t))))))
+  (while true
+    (process-tweets (:tweet (client/retrieve-queues stream)))
+    (Thread/sleep (* 1024 5))))
